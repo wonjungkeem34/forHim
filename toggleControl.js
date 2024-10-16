8b6d8106115e8b0089ed89f0273cde49e014a051
wonjungkeem34 <gu2duck219@gmail.com>
2024-10-16 23:05:08 +0900

    last last last last last toggle update

--- toggleControl.js
+++ toggleControl.js
@@ -11,21 +11,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (matchDetailsDiv) {
           if (matchDetailsDiv.classList.contains("open")) {
-            // matchDetailsDiv가 열려있을 때
             matchDetailsDiv.classList.remove("open");
             // 버튼의 배경 이미지를 열기 이미지로 변경
-            event.target.style.backgroundImage = `url('./data/img/toggle/toggledown.png')`; // 닫을 때 down 이미지로 변경
+            event.target.style.backgroundImage = `./data/img/toggle/toggledown.png`; // Use backticks for template literals
             matchDetailsDiv.style.display = "none";
           } else {
-            // matchDetailsDiv가 닫혀있을 때
             matchDetailsDiv.classList.add("open");
             // 버튼의 배경 이미지를 닫기 이미지로 변경
-            event.target.style.backgroundImage = `url('./data/img/toggle/toggleup.png')`; // 열 때 up 이미지로 변경
+            event.target.style.backgroundImage = `./data/img/toggle/toggleup.png`; // Use backticks for template literals
             matchDetailsDiv.style.display = "block";
           }
         }
+        event.stopPropagation(); // 클릭 이벤트 전파 방지
       }
-      event.stopPropagation(); // 클릭 이벤트 전파 방지
     }
   });
 });
@@ -47,8 +45,8 @@ document.addEventListener("click", function (event) {
       const toggleButton =
         matchDetailsDiv.previousElementSibling.querySelector(".toggle-details");
       if (toggleButton) {
-        // 버튼의 배경 이미지를 닫기 이미지로 변경
-        toggleButton.style.backgroundImage = `url('./data/img/toggle/toggledown.png')`; // 열기 이미지로 변경
+        // 버튼의 배경 이미지를 열기 이미지로 변경
+        toggleButton.style.backgroundImage = `./data/img/toggle/toggledown.png`; // Use backticks for template literals
         toggleButton.style.display = "block";
       }
     });
