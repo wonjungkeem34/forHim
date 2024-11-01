533ebcabeca2d3300b1723f3093e066ec37af287
wonjungkeem34 <gu2duck219@gmail.com>
2024-11-01 22:07:43 +0900

    fix: Retrieve champion image regardless of case sensitivity

--- findChampion.js
+++ findChampion.js
@@ -2,10 +2,16 @@ export async function findChampionImg(championName, index, version) {
   const response = await fetch(
     `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion.json`
   );
+
   const championData = await response.json();
 
-  const championInfo = championData.data[championName];
-  if (championInfo) {
+  // 모든 챔피언 이름을 소문자로 변환한 객체 생성
+  const championKey = Object.keys(championData.data).find(
+    (key) => key.toLowerCase() === championName.toLowerCase()
+  );
+
+  if (championKey) {
+    const championInfo = championData.data[championKey];
     const recentUseChampIconUrl = `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${championInfo.image.full}`;
 
     const recentUseChampElement = document.getElementById(
@@ -19,5 +25,6 @@ export async function findChampionImg(championName, index, version) {
     }
   } else {
     console.error("Champion not found.");
+    console.log(championName);
   }
 }
